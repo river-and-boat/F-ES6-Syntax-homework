3c1b5eaae9ba64b48a210d14546f10b98a41e90f
RiverAndBoat <842714673@qq.com>
2020-09-10 22:30:03 +0800

    finish exercise 1 use recursion finish exercise 2 by calculating relative offset

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,22 @@
-const parseData
+const parseData = ({data, column}) => {
+    const keys = column.map(obj => obj.name);
+    const result = [];
+    data.map(d => {
+        const obj = {}
+        zip(keys, d).map(([key, value])=> obj[key] = value);
+        result.push(obj);
+    })
+    return result;
+}
+
+const zip = ([x,...xs], [y,...ys]) => {
+    if (x === undefined || y === undefined) {
+        return [];
+    } else {
+       return [[x,y], ...zip(xs, ys)];
+    }
+}
+
 export { parseData };
+
+
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,21 @@
-const inject
+const inject = (items, sections) => {
+    const indexes = sections.map(s => s.index);
+    sections.map(s => {
+        items.splice((s.index + calculateRelativeOffSet(indexes, s.index)),
+            0, s.content);
+    });
+    return items;
+}
+
+const calculateRelativeOffSet = (indexes, index) => {
+    let offSetCount = 0;
+    for (let item of indexes) {
+        if (item === index) {
+            return offSetCount;
+        }
+        if (item < index) {
+            offSetCount += 1;
+        }
+    }
+}
 export { inject };
