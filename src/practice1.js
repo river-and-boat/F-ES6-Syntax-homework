e0902cd966053683e46942befc63d31a1a8e8283
kevindongzg <dongkevin94@gmail.com>
2020-09-21 23:24:54 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,8 +1,10 @@
 const parseData = ({data, column}) => {
     const keys = column.map(obj => obj.name);
+    // TODO 使用map做数组与数组的转化，不用声明result，查查map的使用
     const result = [];
     data.map(d => {
         const obj = {}
+        // TODO 没有必要这么写（zip），不易读且map也不是这么用的
         zip(keys, d).map(([key, value])=> obj[key] = value);
         result.push(obj);
     })
--- src/practice2.js
+++ src/practice2.js
@@ -1,3 +1,4 @@
+// TODO 写的过于复杂了，且map不是这么用的，思考先排序sections、倒序插入等入手
 const inject = (items, sections) => {
     const indexes = sections.map(s => s.index);
     sections.map(s => {
